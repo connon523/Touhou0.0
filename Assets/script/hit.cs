13fbf21325b6f2cc5709d509688ae39e18d0ae98
connon523 <a3010205@yahoo.com.tw>
2021-12-27 16:25:23 +0800

    可以碰撞並消除音符

--- Assets/script/hit.cs
+++ Assets/script/hit.cs
@@ -6,17 +6,39 @@ public class hit : MonoBehaviour
 {
     public KeyCode KeyToPress;
 
+    public bool canBepress;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-                                 
-    }
-    
+        transform.position -= new Vector3(1f * Time.deltaTime, 0f);
 
+        if (Input.GetKeyDown(KeyToPress))
+        {
+            if (canBepress)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "good")
+        {
+            canBepress = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "good")
+        {
+            canBepress = false;
+        }
+    }
 }
