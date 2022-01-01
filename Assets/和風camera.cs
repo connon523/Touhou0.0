b7e670466eaa4aee1950d1cfac571ae056177a6f
connon523 <a3010205@yahoo.com.tw>
2022-01-01 15:31:50 +0800

    background update

---
+++ Assets/和風camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 和風camera : MonoBehaviour
+{
+    private float mSpeed = 6.0F;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+        transform.Translate(Vector3.right * Time.deltaTime * mSpeed);
+       
+        if (transform.position.x >= 19.9F)
+        {
+           
+            transform.position = new Vector3(0F, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/山camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private float mSpeed = 6.0F;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        //Translate form right to left  
+        transform.Translate(Vector3.right * Time.deltaTime * mSpeed);
+        // If first background is out of camera view,then show sencond background  
+        if (transform.position.x >= 19.9F)
+        {
+            //We can chenge this value to reduce the wdith between 2 background  
+            transform.position = new Vector3(0F, transform.position.y, transform.position.z);
+        }
+    }
+}
