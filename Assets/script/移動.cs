817d57107edf0a4d6ab32cb6e060966961c0242c
DESKTOP-6JUUAVI\陳柏硯 <4a9k0035@stust.edu.tw>
2022-01-01 22:33:35 +0800

    123

---
+++ Assets/script/移動.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 移動 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 target = new Vector3(transform.position.x,-15, transform.position.z);
+            gameObject.transform.position = target  ;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 target = new Vector3(transform.position.x, -20, transform.position.z);
+            gameObject.transform.position =  target ;
+        }
+    }
+}
