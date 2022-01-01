61f7353a9fd1712a11f902a434cdef57d4464122
connon523 <a3010205@yahoo.com.tw>
2022-01-01 17:13:00 +0800

    no message

---
+++ Assets/script/lilmove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lilmove : MonoBehaviour
+{
+    float radian = 0;
+    float perRadian = 0.03f;
+    float radius = 0.8f;
+    Vector3 oldpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        radian += perRadian;
+
+        float dy = Mathf.Cos(radian) * radius;
+        transform.position = oldpos + new Vector3(0, dy, 0);    
+    }
+}
